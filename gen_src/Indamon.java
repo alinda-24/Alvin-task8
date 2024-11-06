47186b6bf27553bf3c49093ff4ed93311914bc9a
Alvin Johansson <alvinjo@kth.se>
2024-11-06 10:57:52 +0100

    commit

--- gen_src/Indamon.java
+++ gen_src/Indamon.java
@@ -1,76 +1,129 @@
-// File: Indamon.java
-import java.util.List;
-import java.util.ArrayList;
 
-public class Indamon {
+public abstract class Indamon {
     private String name;
     private String type;
     private int strength;
     private int agility;
     private int intelligence;
+    private int score;
 
     public Indamon(String name, String type, int strength, int agility, int intelligence) {
-        // Constructor logic
+        this.name = name;
+        this.type = type;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+        this.score = 0;
     }
 
     public String getName() {
-        // Getter logic
+        return name;
     }
 
     public void setName(String name) {
-        // Setter logic
+        this.name = name;
     }
 
     public String getType() {
-        // Getter logic
+        return type; 
     }
 
     public void setType(String type) {
-        // Setter logic
+        this.type = type;
     }
 
     public int getStrength() {
-        // Getter logic
+        return strength;
     }
 
     public void setStrength(int strength) {
-        // Setter logic
+        this.strength = strength;
     }
 
     public int getAgility() {
-        // Getter logic
+        return agility;
     }
 
     public void setAgility(int agility) {
-        // Setter logic
+        this.agility = agility;
     }
 
     public int getIntelligence() {
-        // Getter logic
+        return intelligence;
     }
-
     public void setIntelligence(int intelligence) {
-        // Setter logic
+        this.intelligence = intelligence;
     }
+    public int getScore() {
+        return score;
+    }
+    public void addScore(int points) {
+        this.score += points;
+    }
+    public abstract int useSpecialAbility();
 
     public String battle(Indamon opponent) {
-        // Battle logic
+        int thisagility = getAgility() + this.useSpecialAbility();
+        int opponentagility = opponent.getAgility() + opponent.useSpecialAbility();
+        int thisstrength = getStrength();
+        int opponentstrength = opponent.getStrength();
+        int thisScore = this.calculateScore();
+        int opponentScore = opponent.calculateScore();
+        System.out.println(this.name + " battles " + opponent.getName() + "!");
+        System.out.println(this.name + " strength " + thisstrength + " and agility: " + thisagility);
+        System.out.println(opponent.getName() + " strength " + opponentstrength + " and agility: " + opponentagility);
+        if (thisstrength > opponentagility){
+            this.addScore(10);
+            return this.name + " has higher strength than opponent has agility and therefore wins and now has a score of: " + thisScore;
+        }
+        else if (thisstrength < opponentagility){
+            System.out.println(opponent.getName() + " has a higher agility and dodges!");
+            System.out.println(opponent.getName() + " counterattacks!");
+            if (opponentstrength > thisagility) {
+                opponent.addScore(10);
+                return opponent.getName() + " Has higher strength and wins and now has a score of: " + opponentScore;
+            }
+            else {
+                return this.name + " Has higher agility and dodges and it ends in a Draw!";
+            }
+        } else {
+            return "it's a draw!";
+        }
     }
 
     public int calculateScore() {
-        // Calculate score logic
+        return strength * 2 + agility * 1 + intelligence * 3;
+    }
+}
+class FireIndamon extends Indamon {
+    public FireIndamon(String name, int strength, int agility, int intelligence) {
+        super(name, "Fire", strength, agility, intelligence);
+    }
+
+    public int useSpecialAbility() {
+        System.out.println(getName() + " uses Fire Blast! (Agility Boost +2)");
+        return 2; 
     }
 }
 
-// File: IndamonLoader.java
-import java.util.List;
-import java.util.ArrayList;
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.FileReader;
 
-class IndamonLoader {
-    public List<Indamon> loadIndamonsFromFile(String filename) throws IOException {
-        // File loading logic
+class WaterIndamon extends Indamon {
+    public WaterIndamon(String name, int strength, int agility, int intelligence) {
+        super(name, "Water", strength, agility, intelligence);
+    }
+
+    public int useSpecialAbility() {
+        System.out.println(getName() + " uses Water Surge! (Agility Boost +3)");
+        return 3; 
+    }
+}
+
+class EarthIndamon extends Indamon {
+    public EarthIndamon(String name, int strength, int agility, int intelligence) {
+        super(name, "Earth", strength, agility, intelligence);
+    }
+    public int useSpecialAbility() {
+        System.out.println(getName() + " uses Earth Shield! (Agility Boost +1)");
+        return 1;
     }
 }
--- gen_src/IndamonBattleGame.java
+++ gen_src/IndamonBattleGame.java
@@ -1,44 +1,79 @@
-IndamonBattleGame.java
-```java
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Random;
+import java.util.Scanner;
 public class IndamonBattleGame {
-
     public static void main(String[] args) {
         IndamonLoader loader = new IndamonLoader();
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
         try {
-            List<Indamon> indamons = loader.loadIndamonsFromFile("indamons.txt");
-            for (int i = 0; i < indamons.size(); i++) {
-                for (int j = i + 1; j < indamons.size(); j++) {
-                    System.out.println(indamons.get(i).battle(indamons.get(j)));
+            List<Indamon> indamons = loader.loadIndamonsFromFile("Indamons.txt");
+            for (Indamon indamon : indamons) {
+                System.out.println("Loaded Indamon: " + indamon.getName() + ", Type: " + indamon.getType());
+            }
+            List<Indamon> enemyIndamons = generateEnemyIndamons();
+            boolean playing = true;
+
+            while (playing){
+                System.out.println("Enter number of steps to take (1-10)");
+                int steps = scanner.nextInt();
+                if (steps < 0 || steps > 10) {
+                    System.out.println("Invalid number of steps. Please choose between 1 and 10.");
+                    continue;
+                }
+                for (int step = 1; step <= steps; step++){
+                    System.out.println("Step " + step);
+                    if (random.nextInt(5) == 1){
+                        System.out.println("An enemy appears!");
+                        Indamon enemyIndamon = enemyIndamons.get(random.nextInt(enemyIndamons.size()));
+                        System.out.println("Which indamon do you wish to use?");
+                        System.out.println("Available Indamons to use:");
+                        for (Indamon indamon : indamons) {
+                            System.out.println("- " + indamon.getName());
+                        }
+                        String indamonuse = scanner.next();
+                        Indamon selectedIndamon = null;
+                        for (Indamon indamon : indamons) {
+                            if (indamon.getName().equalsIgnoreCase(indamonuse)) {
+                                selectedIndamon = indamon;
+                                break;
+                            }
+                            if (selectedIndamon == null) {
+                                System.out.println("No such Indamon found. Please try again.");
+                                continue;
+                            }
+                        }
+                        System.out.println(selectedIndamon.battle(enemyIndamon));
+                        System.out.println("Do you wish to continue y/n?");
+                        String userResponse = scanner.next();
+                        if (userResponse.equals("n")){
+                            System.out.println("Exiting the game. Goodbye!");
+                            playing = false;
+                            break;
+                        } else if (userResponse.equals("y")){
+                            continue;
+                        } else {
+                            System.out.println("Invalid input stoping the game!");
+                            playing = false;
+                            break;
+                        }
+                    }
                 }
             }
         } catch (IOException e) {
-            System.out.println("An error occurred while reading the file: " + e.getMessage());
+            System.out.println("Error loading file: " + e.getMessage());
         }
     }
-}
-```
-
-IndamonLoader.java
-```java
-import java.util.List;
-import java.io.IOException;
-
-public class IndamonLoader {
-
-    public List<Indamon> loadIndamonsFromFile(String filename) throws IOException {
-        // Method to load Indamons from a file
+    private static List<Indamon> generateEnemyIndamons() {
+        List<Indamon> enemies = new ArrayList<>();
+        enemies.add(new FireIndamon("Inferno", 9, 4, 5));
+        enemies.add(new WaterIndamon("AquaFang", 6, 5, 7));
+        enemies.add(new EarthIndamon("TerraClaw", 8, 6, 4));
+        enemies.add(new FireIndamon("Blaze", 7, 5, 6));
+        enemies.add(new WaterIndamon("Hydro", 5, 8, 7));
+        enemies.add(new EarthIndamon("Rocky", 8, 4, 6));
+        return enemies;
     }
 }
-```
-
-Indamon.java
-```java
-public class Indamon {
-
-    public String battle(Indamon opponent) {
-        // Method to initiate a battle between two Indamons
-    }
-}
-```
